482365a7feed8fe73773fc4a730aa7c7add6dd8b
Peter Bengtsson <mail@peterbe.com>
2025-02-10 16:08:29 -0500

    about page

--- src/About.tsx
+++ src/About.tsx
@@ -1,13 +1,63 @@
+import { useNavigate } from "react-router";
+import { useDocumentTitle } from "usehooks-ts";
+import classes from "./about.module.css";
+
 export function About() {
+  useDocumentTitle("About: Spot the Difference");
+  const navigate = useNavigate();
   return (
-    <article>
-      <hgroup>
-        <h2>About: Spot the Difference</h2>
-        <p>A silly goose game</p>
-      </hgroup>
+    <div>
+      <h2>About Spot the Difference</h2>
       <p>
         Made by <a href="https://www.peterbe.com">peterbe</a>
       </p>
-    </article>
+      <p>
+        The code is open at{" "}
+        <a href="https://github.com/peterbe/spot-the-difference">
+          https://github.com/peterbe/spot-the-difference
+        </a>
+        .<br />
+        <a href="https://github.com/peterbe/spot-the-difference/issues/new?template=Blank+issue">
+          Go here to file an issue
+        </a>{" "}
+        if something's not working right.
+      </p>
+      <p>
+        To run it on your laptop, you just need a recent version of{" "}
+        <a href="https://bun.sh/">Bun</a>.
+        <br />
+        In fact, this is how you run it:
+      </p>
+      <ol>
+        <li>
+          <code>
+            git clone https://github.com/peterbe/spot-the-difference.git
+          </code>
+        </li>
+        <li>
+          <code>cd spot-the-diffence</code>
+        </li>
+        <li>
+          <code>bun install</code>
+        </li>
+        <li>
+          <code>bun run dev</code>
+        </li>
+        <li>
+          <code>open http://localhost:3000</code> (in another terminal)
+        </li>
+      </ol>
+
+      <div className={classes.banner}>
+        <button
+          type="button"
+          onClick={() => {
+            navigate("/play");
+          }}
+        >
+          Start!
+        </button>
+      </div>
+    </div>
   );
 }
