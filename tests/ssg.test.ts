023271be4cc487c90644cb86b5ab631e886fc3f4
Peter Bengtsson <mail@peterbe.com>
2025-04-25 08:00:16 -0400

    better test

--- tests/ssg.test.ts
+++ tests/ssg.test.ts
@@ -1,5 +1,23 @@
 import { expect, test } from "bun:test";
 
-test("2 + 2", () => {
-  expect(2 + 2).toBe(4);
+test("about.html should have html and title", async () => {
+  const html = await Bun.file("dist/about.html").text();
+  expect(html).not.toContain('<div id="root"></div>');
+  const match = html.match(/<title>(.*)<\/title>/);
+  expect(match).not.toBeNull();
+  if (match) {
+    const title = match[1];
+    expect(title).toContain("About");
+  }
+});
+
+test("stats.html should have html and title", async () => {
+  const html = await Bun.file("dist/stats.html").text();
+  expect(html).not.toContain('<div id="root"></div>');
+  const match = html.match(/<title>(.*)<\/title>/);
+  expect(match).not.toBeNull();
+  if (match) {
+    const title = match[1];
+    expect(title).toContain("Stats");
+  }
 });
