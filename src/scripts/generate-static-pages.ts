b068a4ef73b54d48d500135042d0cd797836e7e9
Peter Bengtsson <mail@peterbe.com>
2025-05-23 09:57:27 -0400

    inline css (#52)
    * inline css
    
    * width/height on goose

--- src/scripts/generate-static-pages.ts
+++ src/scripts/generate-static-pages.ts
@@ -20,6 +20,25 @@ async function main() {
   const templateHtml = await Bun.file("dist/_index.html").text();
   if (!templateHtml) throw new Error("templateFile is empty");
   const $ = cheerio.load(templateHtml);
+  const links = $('link[rel="stylesheet"]').map((_, el) => {
+    const $el = $(el);
+    const href = $el.attr("href");
+    if (href?.endsWith(".css") && href.startsWith("/")) {
+      return { $el, href };
+    }
+  });
+  for (const { $el, href } of links) {
+    const cssFile = Bun.file(`dist${href}`);
+    if (await cssFile.exists()) {
+      const css = await cssFile.text();
+      let inlineCss = css.replace(/@charset "UTF-8"/i, "");
+      if (inlineCss.startsWith(";")) {
+        inlineCss = inlineCss.slice(1);
+      }
+      $("head").append($("<style>").html(inlineCss));
+      $el.remove();
+    }
+  }
 
   for (const [path, dest, title] of PAGES) {
     await preRenderApp($, path, title);
